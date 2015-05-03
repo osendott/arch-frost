803634243f87ec7dc8800450f2bd1af619b1e800
osendott <william.osendott@gmail.com>
2015-05-03 05:36:36 -0400

    add very simple install script

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#generic install script
+mkdir -p ~/.themes/test/arch-frost
+mv $PWD/arch-frost/ ~/.themes/
+
+mkdir -p ~/.themes/test/arch-frost-light
+mv $PWD/arch-frost-light/ ~/.themes/
+
+mkdir -p ~/.themes/test/arch-frost-shell
+mv $PWD/arch-frost-shell/ ~/.themes/
+
+
+
